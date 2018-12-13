41b313a0cf3769f86a6f063f87851397baa2e51a
Kaj Ström <kaj.strom@kapsi.fi>
2018-12-13 22:17:01 +0200

    Day 13, part two

--- clj/src/clj/day13.clj
+++ clj/src/clj/day13.clj
@@ -126,13 +126,71 @@
 (defn move-carts-ticks-until-crash [track carts]
   (loop [crash nil
          ticks 0]
-    (println ticks)
+    ;(println ticks)
     (if (nil? crash)
       (recur (move-carts-until-crash track carts) (inc ticks))
       crash)))
+
+(defn find-crashed-carts [all-carts]
+  (loop [current-cart (first all-carts)
+         carts-to-test (rest all-carts)]
+    (if-not (nil? current-cart)
+      (let [all-except-current (filter #(not= (:id current-cart) (:id %)) all-carts)
+            crashes (filter #(and (= (:x current-cart) (:x %)) (= (:y current-cart) (:y %))) all-except-current)]
+        ;(println (:id current-cart) (:x current-cart) (:y current-cart) (map :id crashes))
+        (if (empty? crashes)
+          (recur (first carts-to-test) (rest carts-to-test))
+          (conj crashes current-cart)))
+      [])))
+
+(defn move-all-carts [track carts]
+  (loop [cart (first carts)
+         remaining-carts (rest carts)
+         not-crashed-carts carts
+         crashed-carts []
+         ]
+    (if-not (nil? cart)
+      (do
+        (move-cart cart)
+        (let [track-part (get-in track [(:y cart) (:x cart)])]
+          (if (= :intersection track-part) (turn-cart-intersection cart))
+          (if (or (= :curve-l track-part) (= :curve-r track-part)) (turn-cart-curve track-part cart))
+          (let [move-crashed (find-crashed-carts not-crashed-carts)
+                crashed-ids (map :id move-crashed)
+                not-crashed-carts (filter #(not-in? crashed-ids (:id %)) not-crashed-carts)
+                remaining-not-crashed (filter #(not-in? crashed-ids (:id %)) remaining-carts)]
+            ;(println "Not crashed" (map :id not-crashed-carts) track-part)
+            (if (empty? move-crashed)
+              (recur (first remaining-not-crashed) (rest remaining-not-crashed) not-crashed-carts crashed-carts)
+              (recur (first remaining-not-crashed) (rest remaining-not-crashed) not-crashed-carts (concat crashed-carts move-crashed)))
+          )))
+      not-crashed-carts)))
+
+(defn move-all-carts-until-one-left [track carts]
+  (loop [tick 0
+         carts carts]
+    ;(println tick (count carts))
+    (if-not (= 1 (count carts))
+      (let [not-crashed-carts (move-all-carts track carts)]
+        (recur (inc tick) (sort-by :x not-crashed-carts)))
+      carts)))
 
 (defn part-1 []
   (let [track-rows (track-from-file "day13.txt")
         track (vec (map parse-track-row track-rows))
         carts (parse-carts track-rows)]
     (move-carts-ticks-until-crash track carts)))
+
+(defn part-2 []
+  (let [track-rows (track-from-file "day13.txt")
+        track (vec (map parse-track-row track-rows))
+        carts (parse-carts track-rows)]
+    (persistent! (first (move-all-carts-until-one-left track carts)))))
+
+(defn time-results []
+  (time
+    (do
+      (println "Part 1")
+      (time (part-1))
+      (println "Part 2")
+      (time (part-2)))))
