0812581ec21e4a965db36777c0a5d1089d6cda5d
Kaj Ström <kaj.strom@kapsi.fi>
2018-12-24 13:35:54 +0200

    Day 24, part two

--- clj/src/clj/day24.clj
+++ clj/src/clj/day24.clj
@@ -9,6 +9,7 @@
       [])))
 
 (def id (atom 0))
+(def immune-boost (atom 0))
 (defn parse-group [side descr]
   (let [[units hp ap attack-type initiative] (->> (re-find #"([0-9]{1,4}) units each with ([0-9]{1,5}) hit points.*that does ([0-9]{1,5}) ([a-z]{1,}) damage at initiative ([0-9]{1,5})" descr) (drop 1))
         immunities-weaknesses (-> (s/split descr #"\(|\)") second (s/split #";"))]
@@ -17,7 +18,7 @@
                 :side side
                 :units (parse-int units)
                 :hp-per-unit (parse-int hp)
-                :ap-per-unit (parse-int ap)
+                :ap-per-unit (if (= :immune side) (+ (parse-int ap) @immune-boost) (parse-int ap))
                 :attack-type (keyword attack-type)
                 :initiative (parse-int initiative)
                 :weaknesses (parse-keywords "weak to " immunities-weaknesses)
@@ -47,9 +48,13 @@
 
 (defn select-target-for [group available]
   (let [enemies (enemies-of group available)
-        my-damage-to (partial deals-damage-to group)]
-    ;(println (reverse (sort-by (juxt my-damage-to effective-power :initiative) enemies)))
-    (first (reverse (sort-by (juxt my-damage-to effective-power :initiative) enemies)))))
+        my-damage-to (partial deals-damage-to group)
+        selected-target (first (reverse (sort-by (juxt my-damage-to effective-power :initiative) enemies)))]
+    (if-not (nil? selected-target)
+      (if (= 0 (my-damage-to selected-target))
+        nil
+        selected-target)
+      nil)))
 
 (defn select-targets [groups]
   (let [sorted-groups (reverse (sort-by (juxt effective-power :initiative) groups))]
@@ -61,14 +66,17 @@
         selected-targets
         (let [target (select-target-for selecting-group available-targets)
               available-targets (filter #(not= (:id target) (:id %)) available-targets)]
-          (println "Group" (:id selecting-group) "Selected target" (:id target) "Would deal damage" (deals-damage-to selecting-group target))
+          ;(println "Group" (:id selecting-group) "Selected target" (:id target) "Would deal damage" (deals-damage-to selecting-group target))
           (recur (first sorted-groups) (rest sorted-groups) available-targets (assoc selected-targets (:id selecting-group) (:id target))))))))
 
 (defn units-killed-in-attack [attacker defender]
   (let [damage (deals-damage-to attacker defender)
         hp-per-unit (:hp-per-unit defender)
         units-killed (/ (- damage (mod damage hp-per-unit)) hp-per-unit)]
-    (if (> units-killed (:units defender)) (:units defender) units-killed)))
+    ;(println damage hp-per-unit units-killed)
+    ;(if (>= units-killed (:units defender)) (:units defender) units-killed)
+    units-killed
+    ))
 
 (defn remove-destroyed [groups]
   (filter #(>= (:units %) 1) groups))
@@ -84,11 +92,11 @@
               defending-group (first (filter #(= defending-group-id (:id %)) all-groups))]
           (if (nil? defending-group-id)
             (do
-              (println "No target for group" (:id attacking-group))
+              ;(println "No target for group" (:id attacking-group))
               (recur (first remaining-groups) (rest remaining-groups) all-groups)
               )
             (let [units-killed (units-killed-in-attack attacking-group defending-group)]
-              (println "Group" (:id attacking-group) "Attacks" (:id defending-group) "Killing" units-killed)
+              ;(println "Group" (:id attacking-group) "Attacks" (:id defending-group) "Killing" units-killed)
               (assoc! defending-group :units (- (:units defending-group) units-killed))
               (let [remaining-alive-groups (remove-destroyed remaining-groups)
                     all-alive-groups (remove-destroyed all-groups)]
@@ -101,29 +109,69 @@
 (defn infection-won [groups]
   (every? #(= :infection (:side %)) groups))
 
+(defn units-remaining [groups]
+  (apply + (map :units groups)))
+
 (defn fight-until-victory [groups]
-  (loop [groups groups]
+  (loop [groups groups
+         units-rem -99999999]
     (if (or (immune-system-won groups) (infection-won groups))
       groups
       (let [targets (select-targets groups)
             remaining-groups (attack! groups targets)]
-        (recur remaining-groups)))))
+        (if (= units-rem (units-remaining groups))
+               (do
+                 ;(println "Stalemate")
+                 groups)
+               (recur remaining-groups (units-remaining remaining-groups)))))))
 
-(defn units-remaining [groups]
-  (apply + (map :units groups)))
+
 
 (defn part-1 []
   (reset! id 0)
   (let [immune (parse-groups :immune "day24-immune.txt")
         infection (parse-groups :infection "day24-infection.txt")
         groups (concat immune infection)]
-    (doseq [group groups]
-      (println "Immunities" (:immunities group))
-      (println "Weaknesses" (:weaknesses group))
-      )
     (let [remaining-groups (fight-until-victory groups)]
       (if (immune-system-won remaining-groups)
         (println "Immune system won")
         (println "Infection won"))
       (units-remaining remaining-groups))
     ))
+
+(defn print-groups [groups]
+  (doseq [group groups]
+    (println "Group" (:id group) "Side" (:side group) "Units" (:units group))))
+
+(defn boost-immune-until-victory [step start]
+  (loop [boost start]
+    (reset! id 0)
+    (reset! immune-boost boost)
+    ;(println "Immunity boosted by" @immune-boost)
+    (let [immune (parse-groups :immune "day24-immune.txt")
+          infection (parse-groups :infection "day24-infection.txt")
+          groups (concat immune infection)]
+      (let [remaining-groups (fight-until-victory groups)]
+        (if (immune-system-won remaining-groups)
+          (do
+            ;(println "Immune system won")
+            [remaining-groups boost])
+          (do
+            ;(println "Infection won. Units remaining" (units-remaining remaining-groups))
+            ;(print-groups remaining-groups)
+            (recur (+ boost step))))
+        )
+      )))
+
+(defn part-2 []
+  (let [[remaining-groups boost] (boost-immune-until-victory 1 0)]
+    ;(print-groups remaining-groups)
+    (units-remaining remaining-groups)))
+
+(defn time-results []
+  (time
+    (do
+      (println "Part 1")
+      (time (part-1))
+      (println "Part 2")
+      (time (part-2)))))
