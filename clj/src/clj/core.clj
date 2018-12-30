9b3284fc84bdbcddfba21d1c2c8250b3718b4f31
Kaj Ström <kaj.strom@kapsi.fi>
2018-12-30 17:27:37 +0200

    Day 1, get rid of atoms

--- clj/src/clj/core.clj
+++ clj/src/clj/core.clj
@@ -2,9 +2,10 @@
   (:gen-class)
   (:require [clj.day5 :as d5]
             [clj.day6 :as d6]
-            [clj.day22 :as d22]))
+            [clj.day22 :as d22]
+            [clj.day1 :as d1]))
 
 (defn -main
   "I don't do a whole lot ... yet."
   [& args]
-  (println (d22/part-2)))
+  (println (time (d1/part-2))))
--- clj/src/clj/day1.clj
+++ clj/src/clj/day1.clj
@@ -1,7 +1,7 @@
 (ns clj.day1
   (:require [clojure.java.io :refer [resource]]
             [clojure.string :as s]
-            [clj.shared :refer [parse-int]]))
+            [clj.shared :refer :all]))
 
 (defn load-changes [file]
   (->> (resource file)
@@ -12,23 +12,36 @@
 (defn apply-changes [changes]
   (apply + changes))
 
-(defn freq-seen-before [freq freqs-seen]
-  (not (nil? (some #(= freq %) freqs-seen))))
+(defn first-freq-reached-twice [orig-changes]
+  (loop [change (first orig-changes)
+         changes (rest orig-changes)
+         frequency 0
+         freqs-seen []
+         unchecked-frequencies []
+         ]
+    (if (nil? change)
+      (do
+        (let [freqs-seen (sort freqs-seen)
+              seen-before (some #(if (< -1 (java.util.Collections/binarySearch freqs-seen %)) %) unchecked-frequencies)]
+          (if-not (nil? seen-before)
+            seen-before
+            (recur (first orig-changes) (rest orig-changes) frequency (concat freqs-seen unchecked-frequencies) []))))
+      (let [new-frequency (+ frequency change)]
+        (do
+          (recur (first changes) (rest changes) new-frequency freqs-seen (conj unchecked-frequencies new-frequency)))))))
 
-(defn first-freq-reached-twice [changes frequency freqs-seen]
-  (let [changes-left (atom changes)
-        frequency (atom frequency)
-        change (atom 0)
-        freqs-seen (atom freqs-seen)]
-    (while (and (not (empty? @changes-left)) (not (freq-seen-before @frequency @freqs-seen)))
-      (do
-        (swap! freqs-seen conj @frequency)
-        (reset! change (first @changes-left))
-        (reset! changes-left (rest @changes-left))
-        (reset! frequency (+ @frequency @change))
-        ))
-    (if (freq-seen-before @frequency @freqs-seen)
-      @frequency
-      (do
-        (println "Pass completed")
-        (first-freq-reached-twice changes @frequency @freqs-seen)))))
+(defn part-1 []
+  (let [changes (load-changes "day1.txt")]
+    (apply-changes changes)))
+
+(defn part-2 []
+  (let [changes (load-changes "day1.txt")]
+    (first-freq-reached-twice changes)))
+
+(defn time-results []
+  (time
+    (do
+      (println "Part 1")
+      (println (time (part-1)))
+      (println "Part 2")
+      (println (time (part-2))))))
